f7c7e7b6205217dc751f298cf26e20a762609549
tmtakashi <tmtakashi7@gmail.com>
2020-02-05 11:24:14 +0900

    Updates

--- src/components/atoms/chordPalleteOption/chordPalleteOption.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ChordPalleteOption from './chordPalleteOption'
-
-export default { title: 'ChordPalleteOption' };
-
-export const C = () => <ChordPalleteOption>C</ChordPalleteOption>;
