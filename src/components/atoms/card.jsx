2cf0a531cdab967c848fe3638914899494a10d8b
tmtakashi <tmtakashi7@gmail.com>
2020-02-07 18:41:58 +0900

    refactored card

--- src/components/atoms/card.jsx
+++ src/components/atoms/card.jsx
@@ -1,20 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
-const Card = props => {
-  return (
-    <div className="card">
-      {props.children}
-      <style jsx>{`
-        .card {
-          border-radius: 5%;
-          box-shadow: 0px 2px 3px -2px rgba(0, 0, 0, 0.5);
-          min-width: 50%;
-          height: 60%;
-          background-color: #fff;
-        }
-      `}</style>
-    </div>
-  );
-};
+const Card = styled.div`
+  border-radius: 5%;
+  box-shadow: 0px 2px 3px -2px rgba(0, 0, 0, 0.5);
+  min-width: 50%;
+  height: 60%;
+  background-color: #fff;
+`;
 
 export default Card;
