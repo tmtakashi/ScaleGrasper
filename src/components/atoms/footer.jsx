0a98630f3114c4d5860c78d3595b2ee1eb97799c
tmtakashi <tmtakashi7@gmail.com>
2020-02-07 18:53:51 +0900

    refactored footer and header

--- src/components/atoms/footer.jsx
+++ src/components/atoms/footer.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import variables from "../../variables.scss";
+import styled from "styled-components";
+import variables from "../../style-variables";
 
-const Footer = props => {
-  return (
-    <footer>
-      <style jsx>
-        {`
-          footer {
-            background-color: ${variables.primaryColor};
-            width: 100%;
-            height: 3%;
-            position: fixed;
-            bottom: 0;
-          }
-        `}
-      </style>
-    </footer>
-  );
-};
+const Footer = styled.footer`
+  background-color: ${variables.primaryColor};
+  width: 100%;
+  height: 3%;
+  position: fixed;
+  bottom: 0;
+`;
 
 export default Footer;
