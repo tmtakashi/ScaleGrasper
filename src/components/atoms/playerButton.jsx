c878533f9adee3631da43b90ec1ea31e1c595e5f
tmtakashi <tmtakashi7@gmail.com>
2020-02-07 19:13:24 +0900

    refactored playerButton

--- src/components/atoms/playerButton.jsx
+++ src/components/atoms/playerButton.jsx
@@ -1,35 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import variables from "../../variables.scss";
+import variables from "../../style-variables.js";
 
 const PlayerButton = props => {
   return (
-    <div onClick={() => props.onClick()} className="btn">
+    <Button onClick={() => props.onClick()}>
       <FontAwesomeIcon icon={faPlay} />
-      <span>{props.children}</span>
-      <style jsx>
-        {`
-          div {
-            display: flex;
-            align-items: center;
-            border-radius: 4px;
-            border: none;
-            height: 30%;
-            min-width: 15%;
-            padding: 0 2%;
-            color: ${variables.btnTextColor};
-            background: ${variables.primaryColor};
-            cursor: pointer;
-            font-size: 1rem;
-          }
-          div span {
-            margin-left: 10%;
-          }
-        `}
-      </style>
-    </div>
+      <Message>{props.children}</Message>
+    </Button>
   );
 };
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  border: none;
+  height: 30%;
+  min-width: 15%;
+  padding: 0 2%;
+  color: ${variables.btnTextColor};
+  background: ${variables.primaryColor};
+  cursor: pointer;
+  font-size: 1rem;
+`;
+
+const Message = styled.span`
+  margin-left: 10%;
+`;
 
 export default PlayerButton;
